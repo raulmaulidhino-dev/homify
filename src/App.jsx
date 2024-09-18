1d50194787968769f08cfcb603b473df63467296
Raul Maulidhino <raulahmadm314@gmail.com>
2024-09-18 13:57:09 +0700

    fix: debug components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
 import Header from './components/Header.jsx'
 import Hero from './components/Hero.jsx'
+import DifferenceCard from './components/DifferenceCard.jsx'
+
+import Section1_Image from './assets/section-1.png'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <section id="section_1" className="w-full py-12 px-10 flex flex-wrap justify justify-around items-center content-around gap-4">
+        <img src={ Section1_Image } alt="Modern IoT System Illustration" className="max-w-lg inline-block" />
+        <div className="inline-block">
+          <h2 className="mb-[0.5em] text-5xl font-bold">Discover the<br/> Homify <span className="bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent">Differences</span></h2>
+          <p className="text-sm">
+            Homify transforms your home into a smart, connected space with cutting-edge technology, making management, security, and efficiency effortless from your smartphone
+          </p>
+        </div>
+      </section>
+      <section id="difference_cards" className="px-7 flex flex-wrap justify-center content-center items-center gap-2">
+        <DifferenceCard title="Lorem Ipsum" body="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit tempora nesciunt cumque architecto esse illum voluptate praesentium, unde quas qui omnis fugiat incidunt assumenda ipsa sit et magni voluptates totam." />
+        <DifferenceCard title="Lorem Ipsum" body="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit tempora nesciunt cumque architecto esse illum voluptate praesentium, unde quas qui omnis fugiat incidunt assumenda ipsa sit et magni voluptates totam." />
+        <DifferenceCard title="Lorem Ipsum" body="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit tempora nesciunt cumque architecto esse illum voluptate praesentium, unde quas qui omnis fugiat incidunt assumenda ipsa sit et magni voluptates totam." />
+        <DifferenceCard title="Lorem Ipsum" body="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit tempora nesciunt cumque architecto esse illum voluptate praesentium, unde quas qui omnis fugiat incidunt assumenda ipsa sit et magni voluptates totam." />
+      </section>
     </>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 const Hero = () => {
     return (
         <section id="hero" className="py-12 px-10">
-            <h1 className="mx-auto mb-5 text-5xl text-center font-extrabold max-w-5xl">Elevate Your Home Experiences with Cutting-edge <span className="bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent">Smart Technology</span> from Homify.</h1>
+            <h1 className="mx-auto mb-5 text-6xl text-center font-extrabold max-w-5xl">Elevate Your Home Experiences with Cutting-edge <span className="bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent">Smart Technology</span> from Homify.</h1>
             <p className="mx-auto mb-5 text-lg text-gray-3 text-center max-w-lg">Automate your home effortlessly with seamless integration and no tech skills needed</p>
-            <a href="" className="w-fit mx-auto px-10 py-2 rounded-[10px] text-center font-bold bg-gray-2 block">
+            <a href="" className="w-fit mx-auto px-12 py-5 rounded-[35px] text-center text-white text-lg font-bold bg-black block">
                 Upgrade Your Home
             </a>
         </section>
