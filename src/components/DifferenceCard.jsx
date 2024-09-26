e9fb58ea7abbf07fafd0112590142614d8eec669
Raul Maulidhino <raulahmadm314@gmail.com>
2024-09-26 22:31:29 +0700

    feat: add fallback contents in DifferenceCard props

--- src/components/DifferenceCard.jsx
+++ src/components/DifferenceCard.jsx
@@ -1,4 +1,4 @@
-const DifferenceCard = ({ title, body }) => {
+const DifferenceCard = ({ title = "Title", body = "Body" }) => {
     return (
         <article className="p-4 border-2 border-slate-200">
             <h3 className="text-3xl font-bold mb-[0.5em]">{ title }</h3>
