f1d29629ac76d253c08c81f1f3c35963860331d6
Raul Maulidhino <raulahmadm314@gmail.com>
2024-09-28 17:30:25 +0700

    style: update quotes for consistency (use double quotes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import './App.css'
-import Button from './components/Button.jsx'
-import Header from './components/Header.jsx'
-import Hero from './components/Hero.jsx'
-import DifferenceCard from './components/DifferenceCard.jsx'
-import NumbersCard from './components/NumbersCard.jsx'
-import Pricing from './components/Pricing.jsx'
-import FAQ from './components/FAQ.jsx'
-import TrustBox from './components/TrustBox.jsx'
-import Testimony from './components/Testimony.jsx'
-import Footer from './components/Footer.jsx';
-import Services from './components/Services.jsx';
-import Voucher from './components/Voucher.jsx';
+import "./App.css"
+import Button from "./components/Button.jsx"
+import Header from "./components/Header.jsx"
+import Hero from "./components/Hero.jsx"
+import DifferenceCard from "./components/DifferenceCard.jsx"
+import NumbersCard from "./components/NumbersCard.jsx"
+import Pricing from "./components/Pricing.jsx"
+import FAQ from "./components/FAQ.jsx"
+import TrustBox from "./components/TrustBox.jsx"
+import Testimony from "./components/Testimony.jsx"
+import Footer from "./components/Footer.jsx";
+import Services from "./components/Services.jsx";
+import Voucher from "./components/Voucher.jsx";
 
 
-import Section1_Image from './assets/section-1.png'
-import Manage_Your_Home_Image from './assets/manage_your_home.png'
+import Section1_Image from "./assets/section-1.png"
+import Manage_Your_Home_Image from "./assets/manage_your_home.png"
 
 function App() {
   return (
@@ -45,7 +45,7 @@ function App() {
         />
         <DifferenceCard
           title="Innovation that Grows with You"
-          body="Future-proof your home with Homify's scalable and upgradable systems. As technology evolves, your smart home setup can evolve too&mdash;ensuring you're always ahead of the curve"
+          body="Future-proof your home with Homify&rsquo;s scalable and upgradable systems. As technology evolves, your smart home setup can evolve too&mdash;ensuring you&rsquo;re always ahead of the curve"
         />
       </section>
       <section id="the_numbers" className="px-10 py-[6rem]">
@@ -54,8 +54,8 @@ function App() {
         </h2>
         <p className="text-sm text-center max-w-sm mx-auto">Homify efficiently transforms your home with smart solutions for any lifestyle and budget.</p>
         <section id="numbers_card" className="w-[90%] md:w-[80%] mx-auto my-8 grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-8">
-          <NumbersCard number="5.6 hrs" description="saved on average by automating daily tasks with Homify's smart systems." />
-          <NumbersCard number="75%" description="reduction in energy usage with Homify's smart thermostats and lighting solutions compared to traditional systems" />
+          <NumbersCard number="5.6 hrs" description="saved on average by automating daily tasks with Homify&rsquo;s smart systems." />
+          <NumbersCard number="75%" description="reduction in energy usage with Homify&rsquo;s smart thermostats and lighting solutions compared to traditional systems" />
           <NumbersCard number="0.3%" description="of the average household budget invested in Homify to make your home smarter, safer and more efficient." />
           <NumbersCard number="$50K" description="saved in energy costs by one Homify customer over 3 years using our automation and energy management solutions." />
         </section>
@@ -64,7 +64,7 @@ function App() {
         <img src={ Manage_Your_Home_Image } alt="Internet Home Illustration" className="max-w-lg inline-block" />
         <section className="">
           <h2 className="text-5xl font-bold md:max-w-[27rem] mb-[0.5em]"><span className="bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent">Smart</span> Control, One Tap Away</h2>
-          <p className="text-sm max-w-sm mb-[1em]">Manage your entire smart home from one intuitive app whether it's adjusting the lights, setting the temperature, or checking security cameras, Homify puts complete control of your home in the palm of your hand.</p>
+          <p className="text-sm max-w-sm mb-[1em]">Manage your entire smart home from one intuitive app whether it"s adjusting the lights, setting the temperature, or checking security cameras, Homify puts complete control of your home in the palm of your hand.</p>
           <Button link="" label="Manage Your Home" color="text-white" bgColor="bg-black"></Button>
         </section>
       </section>
