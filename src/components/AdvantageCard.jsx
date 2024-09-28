b8052ca7e41614d90290da93db9efaf70f41bc8f
Raul Maulidhino <raulahmadm314@gmail.com>
2024-09-28 21:59:35 +0700

    style: add icons for every AdvantageCard component

--- src/components/AdvantageCard.jsx
+++ src/components/AdvantageCard.jsx
@@ -1,13 +1,21 @@
-const DifferenceCard = ({
+const AdvantageCard = ({
+    Icon,
     title = "Title", 
     body = "Body" 
 }) => {
     return (
-        <article className="p-4 border-2 border-slate-200">
-            <h3 className="text-3xl font-bold mb-[0.5em]">{ title }</h3>
+        <article className="p-4">
+            <div className="mx-auto">
+                { Icon && 
+                <Icon size={50} style={{
+                    marginRight: "auto",
+                    marginLeft: "auto",
+                }}/>}
+            </div>
+            <h3 className="text-3xl text-center font-bold my-[0.5em]">{ title }</h3>
             <p className="text-sm">{ body }</p>
         </article>
     )
 }
 
-export default DifferenceCard
+export default AdvantageCard
