fa084a240f7f4cd817cabb98be168eec82417673
Raul Maulidhino <raulahmadm314@gmail.com>
2024-09-29 22:23:07 +0700

    fix: edit id property

--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -2,7 +2,7 @@ import Hero2Image from "../assets/hero2-img.png"
 
 const Hero2 = () => {
     return (
-        <section id="Hero2" className="w-full px-10 py-[6rem] flex flex-wrap justify-center items-center content-around gap-16">
+        <section id="discover" className="w-full px-10 py-[6rem] flex flex-wrap justify-center items-center content-around gap-16">
             <img src={ Hero2Image } alt="Modern IoT System Illustration" className="md:max-w-md lg:max-w-lg inline-block" />
             <div className="max-w-lg inline-block">
                 <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-5xl font-bold mb-[0.5em] grow">Discover the<br/> Homify <span className="bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent">Differences</span></h2>
