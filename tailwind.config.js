30c3411d7d53e97b948a112c7f39f063ad44a1a2
Raul Maulidhino <raulahmadm314@gmail.com>
2024-09-29 20:50:51 +0700

    fix: remove unused CSS variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        'gray-1': 'var(--gray-1)',
-        'gray-2': 'var(--gray-2)',
-        'gray-3': 'var(--gray-3)',
-      }
     },
   },
   plugins: [],
