e293dc65521b34c7f9f803c7065b0cc7dc36161d
Raul Maulidhino <raulahmadm314@gmail.com>
2025-03-05 23:50:16 +0700

    style: add expand and shrink animations on FAQ Accordion section

--- src/components/FAQ_Accordion.jsx
+++ src/components/FAQ_Accordion.jsx
@@ -12,19 +12,19 @@ const FAQ_Accordion = ({question="Question?", answer="Answer is ..."}) => {
 
     return (
         <section data-aos="fade-right" data-aos-easing="ease-in-sine">
-            <section className="question border-2 rounded-[14px] p-6 flex justify-between items-center gap-4 shadow-lg shadow-slate-300">
+            <section className="question border-2 rounded-[14px] p-6 flex justify-between items-center gap-4 shadow-lg shadow-slate-300" onClick={ toggleAccordion }>
                 <section className="text-sm md:text-base question-text font-bold">
                     { question }
                 </section>
-                <div onClick={ toggleAccordion } className="shrink-0 w-8">
+                <div className="shrink-0 w-8">
                     <img 
                         src={ ArrowDownIcon }
                         className={`transform transition-transform ${isOpen ? 'rotate-180' : ''}`}
                     />
                 </div>
             </section>
-            <section className={`text-sm p-4 mt-[15px] bg-white ${isOpen ? 'block' : 'hidden'}`}>
-                { answer }
+            <section className={`text-sm mt-[15px] bg-transparent ${isOpen ? 'animate-expand' : 'animate-shrink max-h-0 opacity-0'}`}>
+                <p className="p-4">{ answer }</p>
             </section>
         </section>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ export default {
   ],
   theme: {
     extend: {
+      keyframes: {
+        expand: {
+          '0%': { opacity: '0', maxHeight: '0px'},
+          '100%': { opacity: '1', maxHeight: '500px'},
+        },
+        shrink: {
+          '0%': { opacity: '1', maxHeight: '500px'},
+          '100%': { opacity: '0', maxHeight: '0px'},
+        }
+      },
+      animation: {
+        'expand': 'expand 0.75s ease-in',
+        'shrink':'shrink 0.75s ease-out',
+      }
     },
   },
   plugins: [],
